b286d08402673c20ed2a404ba8f875e70e32cc6d
DMNCDLPRT <74143448+DMNCDLPRT@users.noreply.github.com>
2022-07-04 16:09:26 +0800

    Update db.js
    mongodb atlas

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/customerDatabase', {
+    mongoose.connect('mongodb+srv://dmnc:JUQVFTj92vt8qRjt@customerdatabase.pspse.mongodb.net/StoreData?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
